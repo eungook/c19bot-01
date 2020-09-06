c91050ac9730936775da4237e4c314f2436fb9a8
YI Eun Gook <eungook83@gmail.com>
2020-09-07 02:31:23 +0900

    process.env.OPEN_API_SERVICE_KEY 적용

--- openapi.test.js
+++ openapi.test.js
@@ -1,9 +1,10 @@
 const openapi = require('./openapi');
+require('dotenv').config({ path: './test.env' });
 
 describe('openapi', () => {
   describe('조회', () => {
     it('2020년 9월 6일의 신규 확진자 수를 조회합니다.', async () => {
-      const serviceKey = 'RiUr3CxyNpF2k62vdb1NoUqVnxY3DoLbfimGRxncV9MPVkd50oqhN9ScMqkXJ6IwwI6WMTyMSlKQR1pE9US3Tg%3D%3D';
+      const serviceKey = process.env.OPEN_API_SERVICE_KEY;
       const date = new Date('2020-09-06');
       const newDecideCnt = await openapi.init(serviceKey).getNewDecideCnt(date);
       expect(newDecideCnt).toBeGreaterThan(0);
