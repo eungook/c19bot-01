e3b8917b8089b737c09fbd7670b39ff1739f7f85
YI Eun Gook <eungook83@gmail.com>
2020-09-09 02:15:56 +0900

    .env 적용에 따른 테스트 코드 수정

--- openapi.test.js
+++ openapi.test.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
 const openapi = require('./openapi');
 
 describe('openapi', () => {
   describe('조회', () => {
-    it('2020년 9월 6일의 신규 확진자 수를 조회합니다.', async () => {
-      const serviceKey = process.env.OPEN_API_SERVICE_KEY;
-      const date = new Date('2020-09-06');
-      const newDecideCnt = await openapi.init(serviceKey).getNewDecideCnt(date);
+    it('2020년 9월 7일의 신규 확진자 수를 조회합니다.', async () => {
+      const date = new Date('2020-09-07');
+      const newDecideCnt = await openapi.init().getNewDecideCnt(date);
+      console.log({ newDecideCnt });
       expect(newDecideCnt).toBeGreaterThan(0);
-    }, 10000);
+    }, 10000); // 10000: github actions에서 timeout나지 않도록
   });
 });
--- tweet.test.js
+++ tweet.test.js
@@ -3,14 +3,7 @@ const tweet = require('./tweet');
 
 describe('tweet.js', () => {
   it('사용자 정보를 조회합니다.', async () => {
-    const consumer_key = process.env.CONSUMER_KEY;
-    const consumer_secret = process.env.CONSUMER_SECRET;
-    const access_token_key = process.env.ACCESS_TOKEN_KEY;
-    const access_token_secret = process.env.ACCESS_TOKEN_SECRET;
-
-    const userObject = await tweet
-      .init({ consumer_key, consumer_secret, access_token_key, access_token_secret })
-      .show('athlon16');
+    const userObject = await tweet.init().show('athlon16');
     expect(userObject).toBeTruthy();
   });
 });
