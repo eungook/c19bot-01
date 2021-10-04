b584be4936d513a18d615d7ba34832c998b23600
YI Eun Gook <eungook83@gmail.com>
2021-10-04 21:37:49 +0900

    destructDate(): 날짜 정보를 구조 분해하여 리턴한다.

--- util.js
+++ util.js
@@ -1,11 +1,39 @@
 /**
+ * 날짜 정보를 구조 분해하여 리턴한다.
+ * @param {*} date 날짜 문자열
+ * @returns 
+ */
+function destructDate(date) {
+  date = new Date(date);
+  const yyyy = date.getFullYear();
+  const m = date.getMonth() + 1;
+  const mm = m.toString().padStart(2, '0');
+  const d = date.getDate();
+
+  const dd = d.toString().padStart(2, '0');
+  const h = date.getHours();
+  const hh = h.toString().padStart(2, '0');
+  const i = date.getMinutes();
+  const ii = i.toString().padStart(2, '0');
+
+  const s = date.getSeconds();
+  const ss = s.toString().padStart(2, '0');
+  const w = date.getDay();
+  const ww = (['일', '월', '화', '수', '목', '금', '토'])[w];
+
+  return {
+    date, yyyy, m, mm, d,
+    dd, h, hh, i, ii,
+    s, ss, w, ww,
+  };
+}
+
+/**
  * Date를 yyyymmdd 포멧으로 리턴한다.
  * @param date Date 타입
  */
 function getYYYYMMDD(date) {
-  const yyyy = date.getFullYear();
-  const mm = ('0' + (date.getMonth() + 1)).substr(-2);
-  const dd = ('0' + date.getDate()).substr(-2);
+  const { yyyy, mm, dd } = destructDate(date);
   const yyyymmdd = `${yyyy}${mm}${dd}`;
   return yyyymmdd;
 }
