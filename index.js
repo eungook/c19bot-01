f236dbca0d1b1dfc31b713506b41bba36da41c6d
YI Eun Gook <eungook83@gmail.com>
2021-10-04 21:40:20 +0900

    destructDate() 적용

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const Tweet = require('./tweet');
 const OpenAPI = require('./openapi');
-const { getKSTDate } = require('./util');
+const { getKSTDate, destructDate } = require('./util');
 
 (async () => {
   // 확진자 수 조회
   const now = new Date();
   const kst = getKSTDate(now);
-  console.log({ now, kst });
   const newDecideCnt = await OpenAPI.init().getNewDecideCnt(kst);
-  console.log({ newDecideCnt });
+  console.log({ now, kst, newDecideCnt });
 
   // 트윗
-  const yyyy = kst.getFullYear();
-  const m = kst.getMonth() + 1;
-  const d = kst.getDate();
+  const { yyyy, m, d } = destructDate(kst);
   const status = `${yyyy}년 ${m}월 ${d}일 기준 일일 신규 확진자 수는 ${newDecideCnt}명 입니다.`;
   Tweet.init().update(status);
 })();
