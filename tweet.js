89db00789cfd6b6f0844f3aade8bb22296f58007
YI Eun Gook <eungook83@gmail.com>
2021-10-05 22:47:07 +0900

    tweet.js: 간소화

--- tweet.js
+++ tweet.js
@@ -1,32 +1,19 @@
 require('dotenv').config();
 const Twitter = require('twitter-lite');
 
-/**
- * 트윗을 준비합니다.
- */
-function init() {
-  const client = new Twitter({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  });
-
-  const obj = {
-    client,
-    update,
-    show
-  };
-
-  return obj;
-}
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
 
 /**
  * 트윗을 작성합니다.
  * @param status 작성할 트윗 내용
  */
 async function update(status) {
-  const tweet = await this.client.post('statuses/update', { status });
+  const tweet = await client.post('statuses/update', { status });
   return tweet;
 }
 
@@ -35,8 +22,8 @@ async function update(status) {
  * @param screen_name 트위터 아이디
  */
 async function show(screen_name) {
-  const userObject = await this.client.get('users/show', { screen_name });
+  const userObject = await client.get('users/show', { screen_name });
   return userObject;
 }
 
-module.exports = { init };
+module.exports = { update, show };
--- tweet.test.js
+++ tweet.test.js
@@ -3,7 +3,7 @@ const tweet = require('./tweet');
 
 describe('tweet.js', () => {
   it('사용자 정보를 조회합니다.', async () => {
-    const userObject = await tweet.init().show('athlon16');
+    const userObject = await tweet.show('athlon16');
     expect(userObject).toBeTruthy();
   });
 });
