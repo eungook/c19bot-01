f0f998b377be299c53f7ed63a755ff84d887632e
YI Eun Gook <eungook83@gmail.com>
2021-10-05 22:49:20 +0900

    openapi.js: getNewDecideCntDate() 추가 및 코드 개선

--- openapi.js
+++ openapi.js
@@ -1,22 +1,32 @@
 require('dotenv').config();
 const fetch = require('node-fetch');
 const { parseString } = require('xml2js');
-const { getYYYYMMDD } = require('./util');
+const { destructDate } = require('./util');
+const { getKSTHours } = require('./kst-date');
 
 /**
- * OpenAPI와의 통신을 준비합니다.
+ * 현재 시각 기준 오픈 API에서 조회할 수 있는 최신 데이터 날짜를 계산한다.
+ * 참고: 데이터는 보통 KST 기준 오전 10시 이후에 갱신된다.
+ * @see [공공데이터활용지원센터_보건복지부 코로나19 감염 현황 - 공공데이터포털](https://www.data.go.kr/data/15043376/openapi.do)
+ * @returns {Date}
  */
-function init() {
-  const obj = {
-    getNewDecideCnt,
-  };
+function getNewDecideCntDate() {
+  const today = new Date();
+  const h = getKSTHours(today);
+  const isUpdate = (h > 10);
+  if (isUpdate) {
+    return today;
 
-  return obj;
+  } else {
+    const day = (24 * 60 * 60); // h * m * s
+    const yesterday = new Date(today.getTime() - day);
+    return yesterday;
+  }
 }
 
 /**
  * 해당 날짜의 새로운 확진자 수를 리턴한다.
- * @param date 조회할 날짜. Date 타입
+ * @param {Date} 조회할 날짜
  */
 async function getNewDecideCnt(date) {
   const decideCnt1 = await getDecideCnt(date);
@@ -31,13 +41,14 @@ async function getNewDecideCnt(date) {
 
 /**
  * 공공데이터포털 오픈 API에서 조회한 누적 확진자 수를 리턴한다.
- * 참고: 데이터는 보통 오전 10시 이후에 갱신된다.
+ * 참고: 데이터는 보통 KST 기준 오전 10시 이후에 갱신된다.
  * @see [공공데이터활용지원센터_보건복지부 코로나19 감염 현황 - 공공데이터포털](https://www.data.go.kr/data/15043376/openapi.do)
- * @param date 조회할 날짜. Date 타입
+ * @param {Date} date 조회할 날짜
  */
 async function getDecideCnt(date) {
   const SERVICE_KEY = process.env.OPEN_API_SERVICE_KEY;
-  const createDt = getYYYYMMDD(date);
+  const { yyyy, mm, dd } = destructDate(date);
+  const createDt = `${yyyy}${mm}${dd}`;
 
   const url =
     `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson` +
@@ -47,7 +58,7 @@ async function getDecideCnt(date) {
   const decideCnt = await fetch(url)
     .then(response => response.text())
     .then(xml => {
-      let decideCnt;
+      let decideCnt = 0;
       parseString(xml, (err, result) => decideCnt = Number(result.response.body[0].items[0].item[0].decideCnt[0]));
       return decideCnt;
     })
@@ -60,6 +71,6 @@ async function getDecideCnt(date) {
     });
 
   return decideCnt;
-};
+}
 
-module.exports = { init };
+module.exports = { getNewDecideCntDate, getNewDecideCnt };
--- openapi.test.js
+++ openapi.test.js
@@ -1,27 +1,21 @@
-const openapi = require('./openapi');
-const { getKSTDate, destructDate } = require('./util');
+const OpenAPI = require('./openapi');
+const { destructDate } = require('./util');
+const { getKST, getKSTHours } = require('./kst-date');
 
-describe('openapi', () => {
+describe('OpenAPI', () => {
   describe('조회', () => {
     it('2020년 9월 7일의 신규 확진자 수를 조회합니다.', async () => {
       const date = new Date('2020-09-07');
-      const newDecideCnt = await openapi.init().getNewDecideCnt(date);
-      console.log({ date, newDecideCnt });
-      expect(newDecideCnt).toBeTruthy()
+      const newDecideCnt = await OpenAPI.getNewDecideCnt(date);
+      expect(newDecideCnt).toBeTruthy();
     }, 30000); // 30000: github actions에서 timeout나지 않도록
 
-    const now = new Date();
-    let kst = getKSTDate(now);
-    const { h } = destructDate(kst);
-    const isUpdate = (h > 10); // 데이터는 보통 한국 시각 오전 10시 이후에 갱신된다.
-    if (isUpdate == false) {
-      kst = new Date(kst.getTime() - (1 * 24 * 60 * 60)); // 아직 갱신되지 않았으므로 어제의 데이터를 확인한다.
-    }
-    const { yyyy, m, d } = destructDate(kst);
+    const newDecideCntDate = OpenAPI.getNewDecideCntDate();
+    const { yyyy, m, d } = destructDate(newDecideCntDate);
+    console.log({ yyyy, m, d });
 
     it(`${yyyy}년 ${m}월 ${d}일의 신규 확진자 수를 조회합니다.`, async () => {
-      const newDecideCnt = await openapi.init().getNewDecideCnt(kst);
-      console.log({ kst, newDecideCnt });
+      const newDecideCnt = await OpenAPI.getNewDecideCnt(newDecideCntDate);
       expect(newDecideCnt).toBeTruthy();
     }, 30000); // 30000: github actions에서 timeout나지 않도록
   });
