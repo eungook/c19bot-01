055e97b33c4fe40404f1a861d4a65bde9c859b14
YI Eun Gook <eungook83@gmail.com>
2021-10-05 22:55:57 +0900

    코드 개선에 따른 수정

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const Tweet = require('./tweet');
 const OpenAPI = require('./openapi');
-const { getKSTDate, destructDate } = require('./util');
+const { destructDate } = require('./util');
 
 (async () => {
   // 확진자 수 조회
-  const now = new Date();
-  const kst = getKSTDate(now);
-  const newDecideCnt = await OpenAPI.init().getNewDecideCnt(kst);
-  console.log({ now, kst, newDecideCnt });
+  const now = OpenAPI.getNewDecideCntDate();
+  const newDecideCnt = await OpenAPI.getNewDecideCnt(now);
+  console.log({ now, newDecideCnt });
 
   // 트윗
-  const { yyyy, m, d } = destructDate(kst);
+  const { yyyy, m, d } = destructDate(now);
   const status = `${yyyy}년 ${m}월 ${d}일 기준 일일 신규 확진자 수는 ${newDecideCnt}명 입니다.`;
-  Tweet.init().update(status);
+  Tweet.update(status);
 })();
