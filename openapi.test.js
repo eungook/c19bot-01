5d7b5644f697a0ea827c26c3ee6695e850f95ee5
YI Eun Gook <eungook83@gmail.com>
2021-10-06 16:50:08 +0900

    getNewDecideCntDate() 테스트 코드 추가

--- openapi.test.js
+++ openapi.test.js
@@ -1,8 +1,20 @@
 const OpenAPI = require('./openapi');
 const { destructDate } = require('./util');
-const { getKST, getKSTHours } = require('./kst-date');
+const { getKSTHours } = require('./kst-date');
 
 describe('OpenAPI', () => {
+  describe('getNewDecideCntDate()', () => {
+    const today = new Date();
+    const h = getKSTHours(today);
+    const newDecideCntDate = OpenAPI.getNewDecideCntDate();
+    console.log({
+      today,
+      h,
+      newDecideCntDate,
+    })
+    expect(newDecideCntDate).toBeTruthy();
+  });
+
   describe('조회', () => {
     it('2020년 9월 7일의 신규 확진자 수를 조회합니다.', async () => {
       const date = new Date('2020-09-07');
